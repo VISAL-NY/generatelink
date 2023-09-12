955634da3e4ace950e45ce1286d660b3f862b002
visalny <visal.ny@ubill24.com>
2023-09-12 12:08:51 +0700

    init generate link

---
+++ GenerateLink/Controllers/GenerateLinkController.cs
@@ -0,0 +1,35 @@
+﻿using GenerateLink.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace GenerateLink.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class GenerateLinkController : ControllerBase
+    {
+        [HttpPost("transaction/generatelinks")]
+        public async  Task<ActionResult<ResponseGenerateLink?>> GenerateLink(RequestGenerateLink info)
+        {
+            var webUrl = "https://deeplinkweb.vercel.app/#/checkout/";
+            var deepLink = "http://bankdeeplink.net/";
+            string originalData=info.MerchantId +"-"+ info.TransactionId;
+            byte[] enCodeString = Encoding.UTF8.GetBytes(originalData);
+            string base64String = Convert.ToBase64String(enCodeString);
+            var response = new ResponseGenerateLink()
+            {
+                Code="000",
+                Message="Generate Success",
+                Data=new Link()
+                {
+                    WebPaymentUrl=webUrl+$"{base64String}",
+                    MobileDeepLink=deepLink+$"{base64String}",
+                }
+            };
+
+
+            return Ok(response);
+        }
+    }
+}
---
+++ GenerateLink/Model/GenerateLink.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GenerateLink.Model
+{
+
+    public class Link
+    {
+        [JsonPropertyName("web_payment_url")]
+        public string WebPaymentUrl { get; set; } = string.Empty;
+        [JsonPropertyName("mobile_deep_link")]
+        public string MobileDeepLink { get; set; } = string.Empty;
+    }
+    public class RequestGenerateLink
+    {
+        [JsonPropertyName("merchant_id")]
+        public string MerchantId { get; set; } = string.Empty;
+        [JsonPropertyName("transaction_id")]
+        public string TransactionId { get; set; } = string.Empty;
+    }
+
+    public class ResponseGenerateLink
+    {
+        [JsonPropertyName("code")]
+        public string Code { get; set; }=string.Empty;
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = string.Empty;
+        [JsonPropertyName("data")]
+        public Link Data { get; set; }=new Link();
+    }
+}
